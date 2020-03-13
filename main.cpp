16a0dab6b5e8bb1f8dcc0bee7a9eaa8eae9ae007
Gaby <gitlanm@gmail.com>
2020-03-13 20:24:43 +0200

    Skeleton is done
    Program structure is made up

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+  
+};
+
+class Pupil : public Person {
+
+};
+
+class Student : public Person {
+
+};
+
+class Employee : public Student {
+
+};
+
+int main() {
+
+  return 0;
+}
